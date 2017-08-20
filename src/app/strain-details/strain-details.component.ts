480406d2acde5ec54119e1f82f0dc9b94422f34d
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-20 01:19:03 -0700

    add an api call to the OnInit of the strain-detail component to grab the data for the selected strain

--- src/app/strain-details/strain-details.component.ts
+++ src/app/strain-details/strain-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CannabisReportService } from './../services/cannabis-report.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-strain-details',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./strain-details.component.sass']
 })
 export class StrainDetailsComponent implements OnInit {
+  selectedStrainUcpc: string = null;
+  selectedStrain: object = null;
 
-  constructor() { }
+  constructor(
+    public cannabisService: CannabisReportService,
+    public route: ActivatedRoute
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.selectedStrainUcpc = params['id'];
+
+      this.cannabisService.getStrainDetails(this.selectedStrainUcpc)
+        .subscribe(data => this.selectedStrain = data.data);
+    });
   }
 
 }
